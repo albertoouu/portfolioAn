baf7995921a251962e44bb137c307981d8321a20
albertoouu <albertoouu@gmail.com>
2022-04-05 17:15:22 -0500

    first animations

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import './index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt='developer' />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>Front Developer / Physicist</h2>
+        <Link to='/contact' className="flat-button">CONTACT ME</Link>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
